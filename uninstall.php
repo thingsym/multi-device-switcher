033ee17c33b2377207ed34757ba43527d64c6b46
thingsym <thingsym@gmail.com>
2012-03-04 15:29:55 +0900

    fix option name

--- uninstall.php
+++ uninstall.php
@@ -4,7 +4,7 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
 function multi_device_switcher_delete_plugin() {
-	delete_option( 'multi_device_switcher' );
+	delete_option( 'multi_device_switcher_options' );
 }
 
 multi_device_switcher_delete_plugin();
