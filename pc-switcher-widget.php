559a900b704a7ce82d91bf778243ff1a4ea8ba57
thingsym <thingsym@gmail.com>
2019-06-13 17:17:34 +0900

    docs: fix header

--- pc-switcher-widget.php
+++ pc-switcher-widget.php
@@ -1,32 +1,17 @@
 <?php
 /**
  * Widget Name: PC Switcher Widget
- * Plugin URI: https://github.com/thingsym/multi-device-switcher
+ * Plugin URI:  https://github.com/thingsym/multi-device-switcher
  * Description: PC Switcher Widget add-on for the Multi Device Switcher. Use this widget to add the PC Switcher to a widget.
- * Version: 1.6.2
- * Author: thingsym
- * Author URI: http://www.thingslabo.com/
- * License: GPL2
+ * Version:     1.6.2
+ * Author:      thingsym
+ * Author URI:  http://www.thingslabo.com/
+ * License:     GPL2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: multi-device-switcher
  * Domain Path: /languages/
- */
-
-/**
- *     Copyright 2013 thingsym (http://www.thingslabo.com/)
  *
- *     This program is free software; you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation; either version 2 of the License, or
- *     (at your option) any later version.
- *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU General Public License
- *     along with this program; if not, write to the Free Software
- *     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110, USA
+ * @package     Multi_Device_Switcher
  */
 
 /**
