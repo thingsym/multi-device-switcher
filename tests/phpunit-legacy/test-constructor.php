1bdd941c2781a444a5e1f1aa0d041afb7973d0e2
thingsym <thingsym@gmail.com>
2025-01-15 19:01:49 +0900

    test: fix test case

--- tests/phpunit-legacy/test-constructor.php
+++ tests/phpunit-legacy/test-constructor.php
@@ -50,7 +50,7 @@ class Test_Multi_Device_Switcher_Constructor extends WP_UnitTestCase {
 	 * @group constructor
 	 */
 	function constructor() {
-		$this->assertSame( 10, has_action( 'plugins_loaded', array( $this->multi_device_switcher, 'load_plugin_data' ) ) );
+		$this->assertSame( 10, has_action( 'init', array( $this->multi_device_switcher, 'load_plugin_data' ) ) );
 		$this->assertSame( 10, has_filter( 'plugins_loaded', array( $this->multi_device_switcher, 'load_textdomain' ) ) );
 		$this->assertSame( 10, has_filter( 'plugins_loaded', array( $this->multi_device_switcher, 'init' ) ) );
 
